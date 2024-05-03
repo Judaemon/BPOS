3b33fe60ff24c63594e6fa9b1499debc2be755f7
John ray Judaya <jrj@newmediastaff.com>
2024-05-03 13:57:18 +0800

    initial status

---
+++ resources/js/data/status.tsx
@@ -0,0 +1,14 @@
+export const AVAILABLE = {
+    label: 'Available',
+    value: 'available',
+};
+
+export const OUT_OF_STOCK = {
+    label: 'Out of Stock',
+    value: 'out of stock',
+};
+
+export const PRODUCT_STATUS = [
+    AVAILABLE,
+    OUT_OF_STOCK,
+];
