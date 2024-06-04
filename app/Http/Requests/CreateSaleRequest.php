b4e68108957677d7db292f5ace11b86b00815e8f
John ray Judaya <jrj@newmediastaff.com>
2024-06-04 14:02:57 +0800

    added a correct validation message

--- app/Http/Requests/CreateSaleRequest.php
+++ app/Http/Requests/CreateSaleRequest.php
@@ -23,12 +23,20 @@ class CreateSaleRequest extends FormRequest
     {
         return [
             'total_amount' => 'required|numeric|min:1',
+            'payment' => 'required|numeric|min:1|gte:total_amount',
 
             'products' => 'required|array',
             'products.*.product_id' => 'required|exists:products,id',
             'products.*.quantity' => 'required|integer|min:1',
             'products.*.cost' => 'required|numeric|min:1',
             'products.*.item_total' => 'required|numeric|min:1',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'payment.gte' => 'The payment should be greater than or equal to the total amount.',           
         ];
     }
 }
