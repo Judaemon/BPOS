b3845c15145e49cd29425af3d31d7199abfe516a
John Ray Judaya <judayajohnray@gmail.com>
2024-12-01 03:01:26 +0800

    refactor app.jsx for improved readability and formatting

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -10,22 +10,23 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
-    setup({ el, App, props }) {
-        const root = createRoot(el);
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) =>
+    resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+  setup({ el, App, props }) {
+    const root = createRoot(el);
 
-        root.render(
-            <QueryClientProvider client={queryClient}>
-                <App {...props} />
-                <ReactQueryDevtools />
-            </QueryClientProvider>
-        );
-    },
-    progress: {
-        color: '#4B5563',
-    },
+    root.render(
+      <QueryClientProvider client={queryClient}>
+        <App {...props} />
+        {/* <ReactQueryDevtools /> */}
+      </QueryClientProvider>,
+    );
+  },
+  progress: {
+    color: '#4B5563',
+  },
 });
