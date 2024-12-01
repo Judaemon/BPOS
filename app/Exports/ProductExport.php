622a18f9a3255242e523fe06fa31efaed3d6c72a
John Ray Judaya <judayajohnray@gmail.com>
2024-12-01 09:58:35 +0800

    enhance ProductExport class to include custom cell formatting and headings for Excel export

--- app/Exports/ProductExport.php
+++ app/Exports/ProductExport.php
@@ -4,14 +4,63 @@ namespace App\Exports;
 
 use App\Models\Product;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithCustomValueBinder;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use PhpOffice\PhpSpreadsheet\Cell\Cell;
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
+use Maatwebsite\Excel\DefaultValueBinder;
 
-class ProductExport implements FromCollection
+class ProductExport extends DefaultValueBinder implements FromCollection, ShouldAutoSize, WithCustomValueBinder, WithHeadings
 {
+    public function bindValue(Cell $cell, $value)
+    {
+        // Formats the value of the cell
+        if (in_array($cell->getColumn(), ['B', 'C'])) {
+            // Force the value to be treated as text
+            $cell->setValueExplicit($value, DataType::TYPE_STRING);
+
+            return true;
+        }
+
+        // Default behavior for other cells
+        return parent::bindValue($cell, $value);
+    }
+
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
-        return Product::all();
+        $products = Product::query()
+            ->select([
+                'id',
+                'name',
+                // 'image',
+                'description',
+                'cost',
+                'price',
+                'stock',
+                'status'
+            ])
+            ->get();
+
+        return $products;
+    }
+    
+    /**
+     * @return array
+     */
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'Name',
+            'Description',
+            'Cost',
+            'Price',
+            'Stock',
+            'Status'
+        ];
     }
 }
