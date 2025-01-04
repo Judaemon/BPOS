d7efbcfa613d047caea5f4981d6c2d4628c370e2
John Ray Judaya <judayajohnray@gmail.com>
2025-01-05 07:32:27 +0800

    Integrate ProductDialog into ProductItemCard for enhanced product interaction

--- resources/js/Components/Products/ProductItemCard.jsx
+++ resources/js/Components/Products/ProductItemCard.jsx
@@ -7,21 +7,32 @@ import {
   CardTitle,
 } from '@/shadcn/ui/card';
 
+import ProductDialog from './ProductDialog';
+
 export const ProductItemCard = ({ product }) => {
   const { name, price, stock, description, image } = product;
 
   return (
-    <Card className="w-full">
-      <CardHeader>
-        <CardTitle>{name}</CardTitle>
-        <CardDescription>
-          {price} | {stock}
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <img src={`${image}`} alt="product" className="rounded-full" />
-        <CardFooter>{description}</CardFooter>
-      </CardContent>
-    </Card>
+    <ProductDialog
+      product={product}
+      setProduct={() => {
+        console.log('set product');
+      }}
+      action="updating"
+      dialogTrigger={
+        <Card className="w-full transition-transform transform hover:scale-105 hover:shadow-lg hover:cursor-pointer">
+          <CardHeader>
+            <CardTitle>{name}</CardTitle>
+            <CardDescription>
+              {price} | {stock}
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <img src={`${image}`} alt="product" className="rounded-full" />
+            <CardFooter>{description}</CardFooter>
+          </CardContent>
+        </Card>
+      }
+    />
   );
 };
